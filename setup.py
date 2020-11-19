87b4ce8c1261431532f13d66252f89b06ddd6cc3
endalk2 <eb808826@gmail.com>
2020-11-19 16:51:39 +0300

    added package configurations

--- setup.py
+++ setup.py
@@ -1,22 +1,46 @@
 
 import setuptools
+import pathlib
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+BASE_DIR = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (BASE_DIR / 'README.md').read_text(encoding='utf-8')
+
 
 setuptools.setup(
     name="py3collections",
     version="0.0.3",
+
     author="Endalkachew Biruk",
     author_email="eb808826@gmail.com",
+
     description="Collection of python tools for different scenarios.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/endalk200/py_utils",
-    packages=setuptools.find_packages(exclude=['docs', 'tests*']),
+
+    url="https://github.com/endalk200/py_tools",
+
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where="src", exclude=['docs', 'tests*']),
+
     install_requires=[],
+
+    # For example, the following would provide a command called `sample` which
+    # executes the function `main` from this package when invoked:
+    entry_points={  # Optional
+        'console_scripts': [
+            'jpeg=pycollections.scripts:convert_to_jpeg',
+        ],
+    },
+
+    scripts=["./scripts/converter"],
+
     classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+
         'Intended Audience :: Developers',
+        "Intended Audience :: Education",
 
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
@@ -27,6 +51,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
 
         "Operating System :: OS Independent",
+
+        "Topic :: Utilities",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.5',
 )
